ade304cfda997ac9ce017bc2ca83018de4696541
Adam <adamo.b1997@gmail.com>
2020-07-05 22:11:57 +0200

    Add entity framework

--- Star-Wars.API/Startup.cs
+++ Star-Wars.API/Startup.cs
@@ -6,11 +6,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Star_Wars.Infrastructure.Persistence;
 
 namespace Star_Wars.API
 {
@@ -32,6 +34,10 @@ namespace Star_Wars.API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Star Wars - Heroes WebAPI", Version = "v1" });
             });
+
+            services.AddDbContext<StarWarsDbContext>(options => options
+                .UseSqlServer(Configuration.GetConnectionString("DBConnection")));
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
